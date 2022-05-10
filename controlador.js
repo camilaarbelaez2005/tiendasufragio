b074611c8dffe16af6259667703ced0c3ebb807e
unknown <camilaag2705@gmail.com>
2022-05-10 17:13:40 -0500

    Nuevo

--- controlador.js
+++ controlador.js
@@ -2,106 +2,136 @@
 let producto1={
     nombre:"Buso blanco tejido",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/Buso.jfif?alt=media&token=c22cb529-f2dd-4a30-9986-317dbbf4a5fd"
 }
 
 let producto2={
     nombre:"Buso amarillo con tiras",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/Mio.jfif?alt=media&token=3995edaf-6459-4aab-ad08-6796c0fcd52c"
 }
 
 let producto3={
     nombre:"Crop top negro",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/Tiras.jfif?alt=media&token=e4acb831-b7a0-44e0-9a32-3cfd1fe6ec60"
 }
 
 let producto4={
     nombre:"Camisa amplia corta",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto1.jfif?alt=media&token=3bc3e9d9-63d7-4a0e-8498-255a8fe31981"
 }
 
 let producto5={
     nombre:"Zapatos rosados",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto10.jfif?alt=media&token=664c2f38-65f8-4d3f-b04f-42ea9a007e77"
 }
 
 let producto6={
     nombre:"Tenis Plataformas Artu",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto11.jfif?alt=media&token=b9bfb991-b919-4eb2-9aa4-d06da0fc35a2"
 }
 
 let producto7={
     nombre:"Zapatos altos negros",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto12.jfif?alt=media&token=24892334-311c-4a98-a9ed-308ce5c04787"
 }
 
 let producto8={
     nombre:"Enterizo negro en cuerina",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto13.jfif?alt=media&token=64c6b279-f2e8-4ef3-8482-c95d0574177c"
 }
 
 let producto9={
     nombre:"Falda animal print",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto14.jfif?alt=media&token=c9715f47-95f2-4f0a-acc6-084726b62c23"
 }
 
 let producto10={
     nombre:"Falda de jean azul",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto15.jfif?alt=media&token=3c17a509-f3b0-49eb-96c9-393e247c05ac"
 }
 
 let producto11={
     nombre:"Falda corta clara",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto16.jfif?alt=media&token=13827990-0e73-4310-9870-b251375e8f12"
 }
 
 let producto12={
-    nombre:"producto12",
+    nombre:"Short claro tiro alto",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto17.jfif?alt=media&token=eec0ab69-d567-4a8a-96d2-48b212e56ac0"
 }
 
 let producto13={
-    nombre:"producto13",
+    nombre:"Jean claro ancho",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto17.jfif?alt=media&token=eec0ab69-d567-4a8a-96d2-48b212e56ac0"
 }
 
 let producto14={
-    nombre:"producto14",
+    nombre:"Jean azul oscuro",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto3.jfif?alt=media&token=5d957406-3d6a-4e3a-95f9-bb121c1469a1"
 }
 
 let producto15={
-    nombre:"producto15",
+    nombre:"Pantalon holgado",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto4.jfif?alt=media&token=e784733c-d58a-4dae-a935-8087338a5805"
+}
+let producto16={
+    nombre:"Jean Blanco holgado",
+    precio:500000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto6.jfif?alt=media&token=d38d0602-4410-4841-958b-2a3fb5ccb3cd"
+}
+let producto17={
+    nombre:"Pantalon beige",
+    precio:500000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto7.jfif?alt=media&token=77d885ee-c595-45fe-bb8a-a7d7e50289ad"
+}
+let producto18={
+    nombre:"Chanclas negras",
+    precio:500000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto8.jfif?alt=media&token=8ab6f580-d7d4-4c82-8dd7-b9b0807afea9"
+}
+let producto19={
+    nombre:"Tennis blancos con rosado",
+    precio:500000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto9.jfif?alt=media&token=5d4da72e-5d65-42c7-8e47-995e49f88bea"
+}
+let producto20={
+    nombre:"Jean pegado",
+    precio:500000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29138.appspot.com/o/foto5.jfif?alt=media&token=05ac9d4b-de2f-4c54-8cd5-73810310d6e0"
 }
 
 //ARREGLO
